a8e78585bc54d5e1b536807bdbbe7178feb7e1ec
gregbee2 <gregory.beirens@gmail.com>
2018-03-12 14:55:32 +0100

    first build without tests

--- src/array.js
+++ src/array.js
@@ -1,37 +1,36 @@
 'use strict'
-var $b=(function(my){
-	'use strict'
-	my.array=function(arr){
-		return{
-			pushUnique:function(val,unique){
-				if(!!unique===false){
-					return arr.push(val);
-				}
-				else if(arr.indexOf(val)===-1){
-					//unique is set to true and it is really unique
-					return arr.push(val);
-				}
-				else{
-					//unique set but it is not
-					return arr.length;
-				}
-			},
-			groupSequence:function(checkFn){
-				//this function groups elements based on previouselement and checkFn
-				//the function returns an array of arrays with the groupelements in each subArray
-				return arr.reduce(function(result,value,index,array){
-					if(checkFn(array[index-1],value)){
-						//add to last element of result
-						result[result.length-1].push(value);
-					}
-					else{
-						//new subArray
-						result.push([value]);
-					}
-					return result;
-				},[]);
-			}
+function pushUnique(arr,val,unique){
+	if(!!unique===false){
+		return arr.push(val);
+	}
+	else if(arr.indexOf(val)===-1){
+		//unique is set to true and it is really unique
+		return arr.push(val);
+	}
+	else{
+		//unique set but it is not
+		return arr.length;
+	}
+}
+function groupSequence(arr,checkFn){
+	//this function groups elements based on previouselement and checkFn
+	//the function returns an array of arrays with the groupelements in each subArray
+	return arr.reduce(function(result,value,index,array){
+		if(checkFn(array[index-1],value)){
+			//add to last element of result
+			result[result.length-1].push(value);
 		}
+		else{
+			//new subArray
+			result.push([value]);
+		}
+		return result;
+	},[]);
+}
+export default function array(arr){
+	return {
+		pushUnique:pushUnique.bind(null,arr),
+		groupSequence:groupSequence.bind(null,arr)
 	};
-	return my;
-})($b);
+};
+	

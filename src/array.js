34bfe1ad963ead596642e7779be16d772eec1478
gregbee2 <gregory.beirens@gmail.com>
2018-03-12 15:23:40 +0100

    changed groupsequence for first index, groupSequence

--- src/array.js
+++ src/array.js
@@ -13,7 +13,7 @@ function groupSequence(arr,checkFn){
 	//this function groups elements based on previouselement and checkFn
 	//the function returns an array of arrays with the groupelements in each subArray
 	return arr.reduce(function(result,value,index,array){
-		if(checkFn(array[index-1],value)){
+		if(index&&checkFn(array[index-1],value)){
 			//add to last element of result
 			result[result.length-1].push(value);
 		}

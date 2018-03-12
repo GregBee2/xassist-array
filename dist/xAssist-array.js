e9d18663f310ce3f286440b765d4c788abaf671c
gregbee2 <gregory.beirens@gmail.com>
2018-03-12 15:21:35 +0100

    test for undeinfed with first value, groupSequence

--- dist/xAssist-array.js
+++ dist/xAssist-array.js
@@ -1,6 +1,6 @@
 // https://github.com/GregBee2/xassist-array#readme Version 0.0.3.
 // Copyright 2018 undefined.
-// Created on Mon, 12 Mar 2018 14:21:17 GMT.
+// Created on Mon, 12 Mar 2018 14:21:33 GMT.
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :

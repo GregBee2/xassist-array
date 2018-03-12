87241f39a0d6ce846e5c7b7d22435bbc04cbf573
gregbee2 <gregory.beirens@gmail.com>
2018-03-12 15:04:48 +0100

    test added

--- test/array-test.js
+++ test/array-test.js
@@ -1,11 +1,17 @@
 var definition = require("../package.json");
-var main=require("../"+definition.main);
+var { array }=require("../"+definition.main);
 var tape=require("tape");
 
+var testArray=[
+	1,2,3,4,5,6
+]
 
 
-tape("empty test", function(test) {
-	test.ok(true);
-	test.end();
+tape("pushUnique should only push values when unique", function(test) {
+	var t=array(testArray);
+	var result=t.pushUnique(7);
+	test.ok(testArray.length===result && testArray[6]==7,
+		"pushUnique(val) adds nonUnique value to array");
+	test.end();	
 });
 
